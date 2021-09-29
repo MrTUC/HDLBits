7eaa2e6154b8fe4c80957df364a4b72abcae9443
MrTUC <91588167+MrTUC@users.noreply.github.com>
2021-09-29 12:45:10 +0200

    Create Zero.v

---
+++ Getting Started/Zero.v
@@ -0,0 +1,11 @@
+/*
+ * This file could be similar to the previous one. But since the tool sets undefined ports to zero it can just be left empty. This is bad designing habit.
+ */
+
+module top_module(
+    output zero
+);// Module body starts after semicolon
+    
+
+
+endmodule
