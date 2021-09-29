273bf5fd8c23774439b926a29313004c14c9f531
MrTUC <91588167+MrTUC@users.noreply.github.com>
2021-09-29 12:48:41 +0200

    Create Wire.v

---
+++ Verilog Language/Basics/Wire.v
@@ -0,0 +1,7 @@
+/*
+ * In this module we connect the input port to the output port. This is done with assigning the input signal to the output signal.
+ */
+
+module top_module ( input in, output out );
+	assign out = in;
+endmodule
