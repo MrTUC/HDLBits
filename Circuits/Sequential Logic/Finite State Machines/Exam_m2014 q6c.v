31d248190af14d089db2b6c69365da747dd9c592
MrTUC <91588167+MrTUC@users.noreply.github.com>
2022-12-14 04:48:26 +0100

    Update Exam_m2014 q6c.v

--- Circuits/Sequential Logic/Finite State Machines/Exam_m2014 q6c.v
+++ Circuits/Sequential Logic/Finite State Machines/Exam_m2014 q6c.v
@@ -3,32 +3,8 @@ module top_module (
     input w,
     output Y2,
     output Y4);
-    
-    localparam A=6'b000001, B=6'b000010, C=6'b000100, D=6'b001000, E=6'b010000, F=6'b100000;
-    reg [5:0] next_state;
-    wire [5:0] newA, newB, newC, newD, newE, newF, state_in;
-    
-    assign	newA 	= y & A;
-    assign	newB	= y & B;
-    assign	newC	= y & C;
-    assign	newD	= y & D;
-    assign	newE	= y & E;
-    assign	newF	= y & F;
-    assign	state_in	= newA | newB | newC | newD | newE | newF;
-    
-    assign Y2 = next_state[1];
-    assign Y4 = next_state[3];
-    
-    always @ ( * ) begin
-        next_state = 6'b000001;
-        case ( state_in )
-            A: next_state = w ? A : B;
-            B: next_state = w ? D : C;
-            C: next_state = w ? D : E;
-            D: next_state = w ? A : F;
-            E: next_state = w ? D : E;
-            F: next_state = w ? D : C;
-        endcase
-    end
+       
+    assign Y2 = (!w & y[1]);
+    assign Y4 = (w & (y[2] | y[3] | y[5] | y[6]));
        
 endmodule
